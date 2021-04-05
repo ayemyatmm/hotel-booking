3f7f24082fe77d714e9ca14301123ff7543463d6
AyeMyatMyatPhyo <ayemyatmyatphyo@mm.cybermissions.co.jp>
2021-04-05 18:50:05 +0900

    BookingListMenuを追加する

--- hotel/urls.py
+++ hotel/urls.py
@@ -6,10 +6,8 @@ urlpatterns = [
     path('room/', views.RoomListView.as_view(), name= "room"),
     path('room/<int:pk>', views.RoomDetailView.as_view(), name = 'room-detail'),
     path('room/<int:pk>/booking_create/', views.BookingCreate.as_view(),name = 'booking-create'),
-    # path('restaurants/',views.RestaurantsView.as_view(),name='restaurants'),
     path('room/<int:pk>/booking_list', views.BookingListView.as_view(), name = 'booking'),
+    path('booking_list', views.BookingListView.as_view(), name="booking_list"),
     path('room/booking-detail/<int:pk>', views.BookingDetailView.as_view(), name = 'booking-detail'),
     path('room/booking-detail/<int:pk>/service/',views.RoomServiceView.as_view(),name='add-service'),
 ]
-
-   
